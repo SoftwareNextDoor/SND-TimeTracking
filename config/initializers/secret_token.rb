288b62da115d9bcd63dc44b65b7c389a39fb62e1
Carlos Mogollan <emogollan@gmail.com>
2012-01-14 15:38:43 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SndTimetracking::Application.config.secret_token = 'f247a4f3c93c07b8b0cb936550321c889c14b5f9823dea31af3abe6461e2da8de1505da16cc34a2e6d130d012f703bbefa42764191b40825d00c53a00e79ed4c'
